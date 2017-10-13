6919f47b79c08d1a0e2afde1092e1a65768e602d
PradeepTS <pradeepts@sysgain.com>
2017-10-13 19:43:23 +0530

    Update deployment-utility.sh

--- scripts/deployment-utility.sh
+++ scripts/deployment-utility.sh
@@ -149,8 +149,8 @@ function setup_bootnodes
          for var in `seq 0 $(($hostcount - 1 ))`; do
                 NODES[$var]=`echo $alldocs | grep -Po '"hostname":.*?",' | sed -n "$(($var + 1 ))p" | cut -d "," -f1 | cut -d ":" -f2 | tr -d "\""`
                 str1=`echo $alldocs | grep -Po '"bootNodeUrl":.*?",'| cut -d "," -f1 | cut -d '"' -f4 | sed -n "$(($var + 1 ))p" | cut -d " " -f2`
-		str1=`echo $alldocs | grep -Po '"bootNodeUrl":.*?",'| cut -d "," -f1 | cut -d '"' -f4 | sed -n "$(($var + 1 ))p" | cut -d " " -f3 | cut -c1-8`
-		str1=`echo $alldocs | grep -Po '"bootNodeUrl":.*?",'| cut -d "," -f1 | cut -d '"' -f4 | sed -n "$(($var + 1 ))p" | cut -d " " -f4`
+		str2=`echo $alldocs | grep -Po '"bootNodeUrl":.*?",'| cut -d "," -f1 | cut -d '"' -f4 | sed -n "$(($var + 1 ))p" | cut -d " " -f3 | cut -c1-8`
+		str3=`echo $alldocs | grep -Po '"bootNodeUrl":.*?",'| cut -d "," -f1 | cut -d '"' -f4 | sed -n "$(($var + 1 ))p" | cut -d " " -f4`
 		NODESURLS[$var]=$str1 $str2$str3
          done
          echo "Nodes: ${NODES[*]}"
